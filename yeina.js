816838f4bb7ceb88dd5760f3fa41abf18c7ebdb4
Arda Engin Ebcim <66255272+ArdaEnginEbcim@users.noreply.github.com>
2021-01-27 14:25:18 +0300

    Update yeina.js

--- yeina.js
+++ yeina.js
@@ -3,20 +3,17 @@ const Discord = require('discord.js');
 const Meska = require('meska.js');
 const log = require('./src/utils/log');
 const client = new Discord.Client();
-const Commands = global.commands = new Meska.Handler.Command(client);
-const Events = new Meska.Handler.Event(client);
+const Commands = new Meska.Handler.Command('src/commands');
+const Events = new Meska.Handler.Event('src/events');
 require('dotenv').config()
-
-Events.folder('src/events');
-
-Commands.prefix(process.env.PREFIX);
-Commands.owner(process.env.AUTHOR_ID);
-Commands.folder('src/commands');
 
 client.login(process.env.TOKEN).then(
     function() {
-        Events.load();
-        Commands.load();
+        Events.load(client);
+        Commands.load(client);
+        client.on('message' async(message) => {
+            Commands.message(message, { prefix: process.env.PREFIX, owner: process.enc.OWNERID })
+        }
         console.log(log.success + chalk.green('TOKEN ' + ' - ' + 'works successfully!'));
     },
     function(err) {
